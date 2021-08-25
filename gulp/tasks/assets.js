3837435553db3fe9295ff8cd1f2ab9238b63ca68
sergey <erofsey@gmail.com>
2021-08-25 16:54:07 +0600

    Добавил задачу assets

---
+++ gulp/tasks/assets.js
@@ -0,0 +1,21 @@
+import {
+  src,
+  dest,
+  watch,
+  parallel,
+} from 'gulp';
+
+import config from '../config';
+
+const fontsBuild = () => (
+  src(`${config.src.fonts}/**/*`)
+    .pipe(dest(`${config.dist.fonts}`))
+);
+
+export const assetsBuild = parallel(
+  fontsBuild,
+);
+
+export const assetsWatch = () => {
+  watch(`${config.src.fonts}/**/*`, fontsBuild);
+};
